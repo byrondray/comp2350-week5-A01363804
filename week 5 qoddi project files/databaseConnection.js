2b514d5cb8e49b0fa74249b431e4db7b8c3d5c04
DELLXPS\byron <byrondray8@gmail.com>
2024-02-10 16:38:05 -0800

    configure db

--- week 5 qoddi project files/databaseConnection.js
+++ week 5 qoddi project files/databaseConnection.js
@@ -1,29 +1,27 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const is_qoddi = process.env.IS_QODDI || false;
 
 const dbConfigQoddi = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
-	multipleStatements: false
+  host: "sql.freedb.tech",
+  user: "freedb_comp2350_main",
+  password: "XFZSr3p6c25m@qb",
+  database: "freedb_comp2350-A01363804",
+  multipleStatements: false,
 };
 
 const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "database1",
-	multipleStatements: false
+  host: "localhost",
+  user: "root",
+  password: "Password",
+  database: "database1",
+  multipleStatements: false,
 };
 
 if (is_qoddi) {
-	var database = mysql.createPool(dbConfigQoddi);
-}
-else {
-	var database = mysql.createPool(dbConfigLocal);
+  var database = mysql.createPool(dbConfigQoddi);
+} else {
+  var database = mysql.createPool(dbConfigLocal);
 }
 
 module.exports = database;
-		
